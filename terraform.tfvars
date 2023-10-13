38257792b2f2a8449ce99d179725271c951a823a
elijahops1 <141283145+elijahops1@users.noreply.github.com>
2023-10-12 20:53:33 -0400

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+access_key = "AKIAXFXNHZRKL56BTY4P"
+secret_key = "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"
+region = "us-east-1"
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+    description = "This is the access-key for AWS"
+  
+}
+
+variable "secret_key" {
+    description = "This is the secret-key for AWS"
+  
+}
+
+variable "region" {
+    description = "This is the region for AWS"
+  
+}
+
+variable "endpoint" {
+    description = "This is the SNS endpoint"
+  
+}
